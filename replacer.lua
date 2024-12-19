f09f4ce73bb971c427eeb637bab2477d39f71b05
44mira <legolaslada@gmail.com>
2024-12-20 05:01:17 +0800

    format: style
    format
    
    f
    
    for

--- replacer.lua
+++ replacer.lua
@@ -1,41 +1,39 @@
-local function camelToSnake(str)
-    return str:gsub("%u", "_%1"):lower()
+local function camel_to_snake(str)
+  return str:gsub("%u", "_%1"):lower()
 end
 
-local function snakeToCamel(str)
-    return str:gsub("_(%w)", string.upper)
+local function snake_to_camel(str)
+  return str:gsub("_(%w)", string.upper)
 end
 
---[[
-    @brief Replaces all identifiers in the file with the new identifiers (snake_case)
-    @param Identifiers: table<Identifier>
-    @param file: string
-]] --
-local function replacer(Identifiers, file, toSnake)
-    if toSnake == nil then
-        toSnake = true
+---Replaces all identifiers in the file with the new identifiers (snake_case)
+---@param identifiers Identifier[]
+---@param file string
+---@return string
+local function replacer(identifiers, file, to_snake)
+  if to_snake == nil then
+    to_snake = true
+  end
+
+  local new_file = ""
+  local previous_end = 0
+  local new_identifier = ""
+  for i = 1, #identifiers, 1 do
+    local identifier = identifiers[i]
+    local old_identifier = string.sub(file, identifier.index, identifier.index + identifier.length - 1)
+
+    if to_snake then
+      new_identifier = camel_to_snake(old_identifier)
+    else
+      new_identifier = snake_to_camel(old_identifier)
     end
 
-    local newFile = ""
-    local previousEnd = 0
-    local newIdentifier = ""
-    for i = 1, #Identifiers, 1 do
-        local identifier = Identifiers[i]
-        local oldIdentifier = string.sub(file, identifier.index, identifier.index + identifier.length - 1)
+    new_file = new_file .. file:sub(previous_end, identifier.index - 1) .. new_identifier
+    previous_end = identifier.index + identifier.length
+  end
 
-        if toSnake then
-            newIdentifier = camelToSnake(oldIdentifier)
-        else
-            newIdentifier = snakeToCamel(oldIdentifier)
-        end
-
-        newFile = newFile .. file:sub(previousEnd, identifier.index - 1) .. newIdentifier
-        previousEnd = identifier.index + identifier.length
-    end
-
-    newFile = newFile .. file:sub(previousEnd, #file)
-    return newFile
+  new_file = new_file .. file:sub(previous_end, #file)
+  return new_file
 end
-
 
 return replacer
